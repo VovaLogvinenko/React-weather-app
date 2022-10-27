433ea3ab9592d09a994a8224aa2dec3b85934d1a
unknown <vovalogvinenco@gmail.com>
2022-10-27 10:43:14 +0300

    change article to section

--- src/main/left-side/main-footer/MainItem.jsx
+++ src/main/left-side/main-footer/MainItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function MainItem({ item }) {
   return (
-    <article className="main-footer__block">
+    <section className="main-footer__block">
       <h3 className="main-footer__title">{item.dt_txt.slice(10, 16)}</h3>
       <img
         src={`image/main/main-footer/${item.weather[0].icon}.svg`}
@@ -11,7 +11,7 @@ function MainItem({ item }) {
         width="40"
       />
       <p className="main-footer__degree">{Math.round(item.main.temp - 273)}&deg;</p>
-    </article>
+    </section>
   );
 }
 
